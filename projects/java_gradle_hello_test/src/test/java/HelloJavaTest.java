a178c530d9fbeb296c6cb58404e02a86e69fd48e
ChenYikunReal <50993163+ChenYikunReal@users.noreply.github.com>
2020-12-14 02:00:36 +0800

    2020-12-14 基于Gradle构建Java、Scala

---
+++ projects/java_gradle_hello_test/src/test/java/HelloJavaTest.java
@@ -0,0 +1,15 @@
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class HelloJavaTest {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+
+}
